7d96fef06741dfdf597b2967ce0b366220ed1114
April Angelo <aprilangelo@yahoo.com>
2018-03-27 09:43:42 +0800

    mar27

--- changepassword.php
+++ changepassword.php
@@ -6,47 +6,47 @@
   <?php include 'templates/css_resources.php' ?>
 </head>
 
-<body id="login-page" class="valign-wrapper">
-  <div class="container">
-      <div class="valign-wrapper">
-          <div class="card-panel" id="login" class="row">
-            <div class="card-content white-text">
-              <div class="row">
-                <img src="img/logo.png" class="login-logo">
+<body id="login-page">
+  <div class="row">
+  <div class="col s12 m12 l4 offset-l4">
+      <div class="card-panel" id="login" class="row">
+        <div class="card-content white-text">
+          <div class="row">
+          </div>
+          <span class="card-title">
+            <img src="img/logo.png" class="login-logo">
+            <span class="card-title"><h5>Update Your Password</h5></span>
+          </span>
+        <form method="post">
+            <div class="input-field form-field login" data-position="right" data-delay="50" data-html="true" data-tooltip="Password Requirements:<br>
+                - 8-20 characters <br>
+                - At least one uppercase letter (A-Z) <br>
+                - At least one lowercase letter (a-z) <br>
+                - At least one number (0-9) <br>
+                - At least one special character (!@#$%^*())" class="tooltipped input-field col s12">
+                <input  id="newpass" name="newpass" type="password" class="validate">
+                <label for="newpass" id="newpass">New Password</label>
             </div>
-            <div id="passvalidate">
-                <span class="card-title"><h5>Update Your Password</h5></span>
-                <p id="passvalidate">
-                </div>
+            <div class="input-field form-field login">
+              <input id="confirmnewpass" name="confirmnewpass" type="password" class="validate">
+              <label for="confirmnewpass" id="confirmnewpass">Confirm New Password</label>
             </div>
-            <form method="post" name="changep" id="changep">
-              <br>
-              <div class="row">
-                <div data-position="right" data-delay="50" data-html="true" data-tooltip="Password Requirements:<br>
-                  - 8-20 characters <br>
-                  - At least one uppercase letter (A-Z) <br>
-                  - At least one lowercase letter (a-z) <br>
-                  - At least one number (0-9) <br>
-                  - At least one special character (!@#$%^*())" class="tooltipped input-field col s12" id="login">
-                  <input  id="newpass" name="newpass" type="password" class="validate">
-                  <label for="newpass" id="newpass">New Password</label>
-                </div>
-              </div>
-              <div class="row">
-                <div class="input-field col s12">
-                  <input id="confirmnewpass" name="confirmnewpass" type="password" class="validate">
-                  <label for="confirmnewpass" id="confirmnewpass">Confirm New Password</label>
-                </div>
-              </div>
-              <div class="row">
-                <br>
-                 <input title="hi" class="waves-effect waves-light btn-login" id="button-login" name="submit" type="submit" value="Update Password">
-                 <input value = "<?php echo $_SESSION['userid']?>" name="userid" type="hidden">
-              </div>
-              </form>
-            </div>
-        </div>
-  </div>
+
+            <div class="form-field input-field">
+             <button style="width: 100%;" class="waves-effect waves-light btn" id="login" name="submit" type="submit">Update Password</button>
+             <input value = "<?php echo $_SESSION['userid']?>" name="userid" type="hidden">
+
+             <br>
+          </div>
+        </form>
+      </div>
+
+      <!-- end of modal -->
+
+    </div>
+</div>
+</div>
+
 
   <?php
   session_start();
